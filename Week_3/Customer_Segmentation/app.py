b30289218e2881c5d324a864fb0248791971c6fa
Prashant Sharma <prashant.ps9833@gmail.com>
2024-12-13 23:23:27 +0530

    did feature selection and updated app.py

--- Week_3/Customer_Segmentation/app.py
+++ Week_3/Customer_Segmentation/app.py
@@ -15,28 +15,13 @@ st.markdown('<style>body{background-color: Blue;}</style>',unsafe_allow_html=Tru
 st.title("Prediction of Cluster")
 
 with st.form("my_form"):
-    Attrition_Flag=st.number_input(label='Attrition Flag',step=0.001,format="%.6f")
-    Customer_Age=st.number_input(label='Customer Age',step=0.001,format="%.6f")
-    Gender=st.number_input(label='Gender',step=0.01,format="%.2f")
-    Dependent_count=st.number_input(label='Dependent Count',step=0.01,format="%.2f")
-    Education_Level=st.number_input(label='Education Level',step=0.01,format="%.2f")
-    Marital_Status=st.number_input(label='Marital Status',step=0.01,format="%.6f")
-    Income_Category=st.number_input(label='Income Category',step=0.01,format="%.6f")
-    Card_Category=st.number_input(label='Card Category',step=0.1,format="%.6f")
-    Months_on_book=st.number_input(label='Months on book',step=0.1,format="%.6f")
-    Total_Relationship_Count=st.number_input(label='Total Relationship Count',step=0.1,format="%.6f")
-    Months_Inactive_12_mon=st.number_input(label='Months Inactive 12_mon',step=1)
-    Contacts_Count_12_mon=st.number_input(label='Contacts Count 12 mon',step=1)
     Credit_Limit=st.number_input(label='Credit Limit',step=0.1,format="%.1f")
     Total_Revolving_Bal=st.number_input(label='Total Revolving Bal',step=0.01,format="%.6f")
     Avg_Open_To_Buy=st.number_input(label='Avg Open To Buy',step=0.01,format="%.6f")
-    Total_Amt_Chng_Q4_Q1=st.number_input(label='Total Amt Chng Q4 Q1',step=0.01,format="%.6f")
     Total_Trans_Amt=st.number_input(label='Total Trans Amt',step=1)
     Total_Trans_Ct=st.number_input(label='Total Trans Ct',step=1)
-    Total_Ct_Chng_Q4_Q1=st.number_input(label='Total Ct Chng Q4 Q1',step=1)
-    Avg_Utilization_Ratio=st.number_input(label='Avg Utilization Ratio',step=1)
 
-    data=[[Attrition_Flag,Customer_Age,Gender,Dependent_count,Education_Level,Marital_Status,Income_Category,Card_Category,Months_on_book,Total_Relationship_Count,Months_Inactive_12_mon,Contacts_Count_12_mon,Credit_Limit,Total_Revolving_Bal,Avg_Open_To_Buy,Total_Amt_Chng_Q4_Q1,Total_Trans_Amt,Total_Trans_Ct,Total_Ct_Chng_Q4_Q1,Avg_Utilization_Ratio]]
+    data=[[Credit_Limit,Total_Revolving_Bal,Avg_Open_To_Buy,Total_Trans_Amt,Total_Trans_Ct]]
 
     submitted = st.form_submit_button("Submit")
     
